b3d600250a89adbf9dc15f8d39b6bff58855b785
Jacques de Laval <chucky@wrutschkow.org>
2012-11-14 19:21:05 +0100

    Icon stuff

---
+++ render_bitmap_icons.sh
@@ -0,0 +1,5 @@
+convert +antialias -background transparent icon.svg -resize 512x512 icon512.png
+convert +antialias -background transparent icon.svg -resize 256x256 icon256.png
+convert +antialias -background transparent icon.svg -resize 128x128 icon128.png
+convert +antialias -background transparent icon.svg -resize 80x80 icon80.png
+convert +antialias -background transparent icon.svg -resize 64x64 icon64.png
